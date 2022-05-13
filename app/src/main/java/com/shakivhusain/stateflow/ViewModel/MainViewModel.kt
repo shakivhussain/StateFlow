7afa0b024536d7032e64ddf5c94b2e645751433e
shakibmansoori <sakib@joinzorro.com>
2022-05-13 14:32:34 +0530

    State and shared flow added

--- app/src/main/java/com/shakivhusain/stateflow/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/shakivhusain/stateflow/ViewModel/MainViewModel.kt
@@ -1,13 +1,51 @@
 package com.shakivhusain.stateflow.ViewModel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
     private val _counter: MutableStateFlow<Int> = MutableStateFlow(0)
-    val counter:StateFlow<Int> = _counter
+
+    // Emit the data in state flow
+    val counter: StateFlow<Int> = _counter
+
+    private val _liveData = MutableLiveData("Hello world")
+    val liveData: LiveData<String> = _liveData
+
+    private val _stateFlow = MutableStateFlow("Hello World")
+    val stateFlow = _stateFlow.asStateFlow()
+
+    private val _sharedFlow = MutableSharedFlow<String>()
+    val sharedFlow = _sharedFlow.asSharedFlow()
+
+    fun triggerLiveData() {
+        _liveData.value = "Live Data"
+    }
+
+    fun triggerStateFlow() {
+        _stateFlow.value = "State Flow"
+    }
+
+    fun triggerFlow(): Flow<String> {
+        return flow {
+            repeat(5){
+                emit("Item : $it")
+                delay(1000L)
+            }
+        }
+    }
+
+    fun triggerSharedFlow(){
+        viewModelScope.launch {
+            _sharedFlow.emit("Shred Flow")
+        }
+    }
 
     fun incrementState() {
         _counter.value++
@@ -17,4 +55,6 @@ class MainViewModel : ViewModel() {
     fun decrementState() {
         _counter.value--
     }
+
+
 }
